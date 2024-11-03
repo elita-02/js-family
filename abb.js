7aba8050cda9343da329f3074b4fc22931cc807d
elita-02 <jyldyzbekovna04@outlook.com>
2024-11-03 14:44:22 +0600

    first commit

---
+++ abb.js
@@ -0,0 +1,12 @@
+const family=prompt("Сен канча бир туугансын?")
+
+let name="Альбина"
+let na='Шайырбек'
+let me='Алия'
+let em='Оомат'
+ 
+console.log(`Мен ${family} бир тууганмын`);
+console.log(`Менин биринчи синдимдин ысымы ` + name );
+console.log(`Экинчи бир тууганымдын аты ` +na);
+console.log(`Менин экинчи синдимдин аты ` + me);
+console.log(`Менин тортунчу бир тууганымдын аты ` +em);
